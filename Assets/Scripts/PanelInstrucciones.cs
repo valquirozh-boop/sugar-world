84c80e7be594cdb8f7ad68f4fd1c2a6a01f26593
saris7ing <sarisecheverry7@gmail.com>
2026-05-19 18:03:50 -0500

    funcion boton comenzar invalida

--- Assets/Scripts/PanelInstrucciones.cs
+++ Assets/Scripts/PanelInstrucciones.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class PanelInstrucciones : MonoBehaviour
 {
-    public GameObject panelInstrucciones;
+    // Esta es la variable que verás en el Inspector para arrastrar el panel
+    public GameObject panelInstrucciones; 
 
     void Start()
     {
+        // Al iniciar el juego, forzamos la pausa y mostramos el panel
         Time.timeScale = 0f;
         panelInstrucciones.SetActive(true);
     }
 
     public void CerrarInstrucciones()
     {
+        // Al pulsar el botón, ocultamos el panel y reanudamos el tiempo
         panelInstrucciones.SetActive(false);
         Time.timeScale = 1f;
     }
