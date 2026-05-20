6dc3b33be16ffb510792e6360363be11f41275a9
saris7ing <sarisecheverry7@gmail.com>
2026-05-19 19:42:21 -0500

    panel de pausa

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    [SerializeField] private int points = 10;
+    public enum TipoColeccionable { Dona, Pastel }
+    public TipoColeccionable tipo;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        ScoreManager.instance?.AddPoints(points);
+
+        if (tipo == TipoColeccionable.Dona)
+        {
+            ScoreManager.instance?.AddPoints(10);
+            ScoreManager.instance?.RecogerDona();
+        }
+        else if (tipo == TipoColeccionable.Pastel)
+        {
+            ScoreManager.instance?.AddPoints(20);
+            ScoreManager.instance?.RecogerPastel();
+        }
+
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
 
     [SerializeField] private Text scoreText;
+
+    public TextMeshProUGUI textDonas;
+    public TextMeshProUGUI textPasteles;
+
     private int score;
+    private int donasRecogidas = 0;
+    private int pastelRecogidos = 0;
+    private int totalDonas = 10;
+    private int totalPasteles = 20;
 
     private void Awake()
     {
@@ -14,11 +23,35 @@ public class ScoreManager : MonoBehaviour
         instance = this;
     }
 
+    void Start()
+    {
+        if (textDonas != null)
+            textDonas.text = "Donas: 0/10";
+        if (textPasteles != null)
+            textPasteles.text = "Pasteles: 0/20";
+        if (scoreText != null)
+            scoreText.text = "Puntos: 0";
+    }
+
     public void AddPoints(int points)
     {
         score += points;
         if (scoreText != null)
             scoreText.text = "Puntos: " + score;
+    }
+
+    public void RecogerDona()
+    {
+        donasRecogidas++;
+        if (textDonas != null)
+            textDonas.text = "Donas: " + donasRecogidas + "/10";
+    }
+
+    public void RecogerPastel()
+    {
+        pastelRecogidos++;
+        if (textPasteles != null)
+            textPasteles.text = "Pasteles: " + pastelRecogidos + "/20";
     }
 
     public int GetScore() => score;
