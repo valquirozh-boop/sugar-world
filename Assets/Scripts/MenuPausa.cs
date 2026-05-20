667311fe28d7b3163d30420f56413838ec9630a4
saris7ing <sarisecheverry7@gmail.com>
2026-05-19 20:50:45 -0500

    funcion boton pausa y continuar

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject panelPausa;
+
+    void Start()
+    {
+        panelPausa.SetActive(false);
+    }
+
+    public void AbrirPausa()
+    {
+        panelPausa.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Continuar()
+    {
+        panelPausa.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void VolverAlMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu_Principal");
+    }
+}
