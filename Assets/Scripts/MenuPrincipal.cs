bfd88d9383fcd9a946936997d9e60afed84eb685
saris7ing <sarisecheverry7@gmail.com>
2026-05-23 18:18:48 -0500

    mejora panel de instrucciones, y arreglo de errores

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -3,15 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
-    // Aquí conectaremos tus paneles de galleta en el Inspector
     public GameObject panelCreditos;
     public GameObject panelConfig;
+    public GameObject panelInstrucciones;
+    public GameObject contenedorMenu;
 
     void Start()
     {
-        // Esto hace que los paneles empiecen apagados al darle Play
-        if(panelCreditos != null) panelCreditos.SetActive(false);
-        if(panelConfig != null) panelConfig.SetActive(false);
+        if (panelCreditos != null) panelCreditos.SetActive(false);
+        if (panelConfig != null) panelConfig.SetActive(false);
+        if (panelInstrucciones != null) panelInstrucciones.SetActive(false);
+    }
+
+    public void AbrirInstrucciones()
+    {
+        if (contenedorMenu != null) contenedorMenu.SetActive(false);
+        if (panelInstrucciones != null) panelInstrucciones.SetActive(true);
     }
 
     public void BotonJugar()
@@ -19,29 +26,13 @@ public class MenuPrincipal : MonoBehaviour
         SceneManager.LoadScene("Nivel1");
     }
 
-    public void AbrirCreditos()
-    {
-        panelCreditos.SetActive(true);
-    }
-
-    public void CerrarCreditos()
-    {
-        panelCreditos.SetActive(false);
-    }
-
-    public void AbrirConfig()
-    {
-        panelConfig.SetActive(true);
-    }
-
-    public void CerrarConfig()
-    {
-        panelConfig.SetActive(false);
-    }
+    public void AbrirCreditos() { panelCreditos.SetActive(true); }
+    public void CerrarCreditos() { panelCreditos.SetActive(false); }
+    public void AbrirConfig() { panelConfig.SetActive(true); }
+    public void CerrarConfig() { panelConfig.SetActive(false); }
 
     public void BotonSalir()
     {
-        Debug.Log("El jugador ha salido");
         Application.Quit();
     }
 }
