688a37c02ba0f43c086a40817a30b40d613c0535
saris7ing <sarisecheverry7@gmail.com>
2026-05-24 20:09:19 -0500

    errores

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -7,18 +7,27 @@ public class MenuPausa : MonoBehaviour
 
     public void AbrirPausa()
     {
+        // Diagnóstico: si ves esto en la consola, el botón funciona
+        Debug.Log("<color=red>¡BOTÓN PULSADO!</color>");
+
         if (panelPausa != null)
         {
             panelPausa.SetActive(true);
             Time.timeScale = 0f;
+        }
+        else
+        {
+            Debug.LogError("El panelPausa no está asignado en el Inspector.");
         }
     }
 
     public void Continuar()
     {
         if (panelPausa != null)
+        {
             panelPausa.SetActive(false);
-        Time.timeScale = 1f;
+            Time.timeScale = 1f;
+        }
     }
 
     public void IrAlMenu()
